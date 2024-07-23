6d97dc268737a88836db3a7ea93a8f1e5330aa0f
TheFunnyPapa <gabrielragy@gmail.com>
2024-07-23 19:28:01 +0300

    updated navbar with logos

--- src/app/containers/navbar-container/navbar.component.ts
+++ src/app/containers/navbar-container/navbar.component.ts
@@ -1,10 +1,9 @@
 // navbar.component.ts
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'navbar-container',
   standalone: true,
@@ -12,4 +11,27 @@ import {MatButtonModule} from '@angular/material/button';
   templateUrl: './navbar.component.html',
 })
 export class NavbarContainerComponent {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {}
+
+  navigateToSection(sectionId: string): void {
+    if (this.router.url !== '/') {
+      // If not on home page, navigate to home first
+      this.router.navigate(['/'], { fragment: sectionId }).then(() => {
+        this.scrollToSection(sectionId);
+      });
+    } else {
+      // If already on home page, just scroll
+      this.scrollToSection(sectionId);
+    }
+  }
+
+  private scrollToSection(sectionId: string): void {
+    const element = this.document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  }
 }
