a9b35bd02d4e3f42184d357242aac45fadc5ec1a
TheFunnyPapa <gabrielragy@gmail.com>
2024-08-23 12:08:05 -0400

    temp before build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-B4YJTM4K.mjs";import"./chunk-BSNAHYED.mjs";import"./chunk-GLCXFXCE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FEBRMWPS.mjs";import"./chunk-BSNAHYED.mjs";import"./chunk-GLCXFXCE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
